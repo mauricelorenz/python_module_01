2b5ae1d109618d901193fa4b3461625c3eebfa69
Maurice Lorenz <me@mauricelorenz.com>
2026-01-16 12:51:02 +0100

    Initial commit

---
+++ ex0/ft_garden_intro.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+def main() -> None:
+    """Run the main program."""
+    name = "Rose"
+    height = "25"
+    age = "30"
+    print("=== Welcome to My Garden ===")
+    print(f"Plant: {name}\nHeight: {height}cm\nAge: {age} days")
+
+
+if __name__ == "__main__":
+    main()
