5c88ca0ba93713a9a0c6c24d10d1cd455faaba92
Maurice Lorenz <me@mauricelorenz.com>
2026-01-17 12:41:24 +0100

    Finalize ex2

--- ex2/ft_plant_growth.py
+++ ex2/ft_plant_growth.py
@@ -16,12 +16,27 @@ class Plant:
         self.age = age
 
     def grow(self, amount: int) -> None:
+        """Increment the height of the Plant.
+
+        Args:
+            amount: height in cm to be added
+        """
         self.height += amount
 
     def increment_age(self, amount: int) -> None:
+        """Increment the age of the Plant.
+
+        Args:
+            amount: age in days to be added
+        """
         self.age += amount
 
     def get_info(self) -> str:
+        """Give an overview about the Plant.
+
+        Returns:
+            {name}: {height}cm, {age} days old
+        """
         return f"{self.name}: {self.height}cm, {self.age} days old"
 
 
@@ -41,6 +56,7 @@ def main() -> None:
     print(f"=== Day {day_end} ===")
     for plant in plants:
         print(plant.get_info())
+        print(f"Growth this week: +{day_end - day_start}cm")
 
 
 if __name__ == "__main__":
